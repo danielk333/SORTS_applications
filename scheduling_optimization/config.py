9779f81c9b29895d2f56adbb92e3496099e3ab57
danielk <daniel.kastinen@irf.se>
2021-05-13 16:57:18 +0200

    finished a simple optim of schedule algorithm

--- scheduling_optimization/config.py
+++ scheduling_optimization/config.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import pathlib
 import numpy as np
 
 import sorts
@@ -20,13 +20,31 @@ obj = sorts.SpaceObject(
     mu0 = 0,
     epoch = 53005.0,
     parameters = dict(
-        d = 1.0,
+        d = 0.1,
     ),
 )
+epoch = obj.epoch
 
 radar = sorts.radars.eiscat3d
 
 t = np.arange(0,3600*24,5.0)
+states = obj.get_state(t)
+passes = radar.find_passes(t, states)
+passes = sorts.passes.group_passes(passes)[0][0] #pick first pass
+
+for ind, ps in enumerate(passes):
+    if ind == 0:
+       t_min_min = ps.start()
+    else:
+        if ps.start() < t_min_min:
+            t_min_min = ps.start()
+
+t_min_min -= 20.0
+
+#set object state to right at start of pass
+obj.propagate(t_min_min)
+
+t = np.arange(0,20*60.0,5.0)
 states = obj.get_state(t)
 passes = radar.find_passes(t, states)
 passes = sorts.passes.group_passes(passes)[0][0] #pick first pass
@@ -41,8 +59,12 @@ for ind, ps in enumerate(passes):
         if ps.end() < t_max:
             t_max = ps.end()
 
+t_pass = t_max - t_min
 
 interpolator = sorts.interpolation.Legendre8(states, t)
+
+variables = ['x','y','z','vx','vy','vz']
+deltas = [1e-4]*3 + [1e-6]*3
 
 tracker = sorts.controller.Tracker(
     radar = radar, 
@@ -50,6 +72,8 @@ tracker = sorts.controller.Tracker(
     ecefs = None,
 )
 
+
+err_pth = pathlib.Path('/home/danielk/IRF/IRF_GITLAB/SORTS/examples/data/')
 
 class TrackScheduler(
         sorts.scheduler.StaticList, 
@@ -71,6 +95,41 @@ class TrackScheduler(
         tracker.ecefs = ecefs_select[:3,:]
         tracker.t = t_select
 
+    def determine_orbit(self):
+        err = sorts.errors.LinearizedCodedIonospheric(
+            self.radar.tx[0], 
+            seed=123, 
+            cache_folder=err_pth,
+        )
+
+        datas = []
+
+        for rxi in range(len(self.radar.rx)):
+            data, J_rx = scheduler.calculate_observation_jacobian(
+                passes[rxi], 
+                space_object=obj, 
+                variables=variables, 
+                deltas=deltas,
+                snr_limit=False,
+            )
+            datas.append(data)
+
+            r_stds_tx = err.range_std(data['range'] - datas[0]['range'], data['snr'])
+            v_stds_tx = err.range_rate_std(data['snr'])
+
+            Sigma_m_diag_tx = np.r_[r_stds_tx**2, v_stds_tx**2]
+
+            if rxi > 0:
+                J = np.append(J, J_rx, axis=0)
+                Sigma_m_diag = np.append(Sigma_m_diag, Sigma_m_diag_tx, axis=0)
+            else:
+                J = J_rx
+                Sigma_m_diag = Sigma_m_diag_tx
+
+        Sigma_m_inv = np.diag(1.0/Sigma_m_diag)
+        Sigma_orb = np.linalg.inv(np.transpose(J) @ Sigma_m_inv @ J)
+
+        return Sigma_orb, datas
 
 
 
--- scheduling_optimization/main.py
+++ scheduling_optimization/main.py
@@ -1,30 +1,111 @@
 #!/usr/bin/env python
-import pathlib
 import numpy as np
 import matplotlib.pyplot as plt
+from tabulate import tabulate
+from tqdm import tqdm
 
 import sorts
 
 #local
-import od
 import config
 
-err_pth = pathlib.Path('/home/danielk/IRF/IRF_GITLAB/SORTS/examples/data/')
 
-print('Scheduling for object:')
-print(config.obj)
+def cost_function(Sigma_orb, num):
+    
+    stds = np.sqrt(np.diag(Sigma_orb))
+    pos_err = np.mean(stds[:3])
+    vel_err = np.mean(stds[3:])
 
-print('First set of passes [all rx stations]')
-for ps in config.passes:
-    print(ps)
+    pos_cost = 1#per m
+    vel_cost = 100#per m/s
+    time_cost = 500#per s
 
-config.scheduler.set_observations(num=10, frac=1)
+    time_used = num*config.tracker.t_slice
 
-Sigma_orb, data = config.scheduler.determine_orbit()
+    return pos_err*pos_cost + vel_err*vel_cost + time_used*time_cost
 
-fig, ax = sorts.plotting.observed_parameters(
-    [data[0]],
-    passes=[config.passes[0]], 
-    snrdb_lim = 10.0
-)
-plt.show()
+
+def test_scheduling_set():
+
+    print('Scheduling for object:')
+    print(str(config.obj) + '\n')
+
+    print('First set of passes [all rx stations]')
+    for ps in config.passes:
+        print(ps)
+
+    print('\n' + 'Setting num=10, frac=0.8')
+    config.scheduler.set_observations(num=10, frac=0.8)
+
+    print('\nObserve and determine orbit\n')
+    Sigma_orb, data = config.scheduler.determine_orbit()
+
+    print(f'Cost of observation: {cost_function(Sigma_orb,10)}\n')
+
+    header = ['']+config.variables
+
+    list_sig = (Sigma_orb).tolist()
+    list_sig = [[var] + row for row,var in zip(list_sig, header[1:])]
+
+    print(tabulate(list_sig, header, tablefmt="simple"))
+
+    for ind, var in enumerate(config.variables):
+        if ind > 2:
+            unit = 'km/s'
+        else:
+            unit = 'km'
+        print(f'{var}-std: {np.sqrt(Sigma_orb[ind,ind])*1e-3:.2f} {unit}')
+
+    fig, ax = sorts.plotting.observed_parameters(
+        [data[0]],
+        passes=[config.passes[0]], 
+        snrdb_lim = 10.0
+    )
+
+    plt.show()
+
+
+def get_param_cost(num, frac):
+    config.scheduler.set_observations(num=num, frac=frac)
+    Sigma_orb, _ = config.scheduler.determine_orbit()
+    return cost_function(Sigma_orb,num)
+
+def grid_search_cost_function(n_steps, f_steps):
+
+    max_n = int(config.t_pass/config.tracker.t_slice)
+
+    N, F = np.meshgrid(
+        n_steps,
+        f_steps, 
+        sparse=False, 
+        indexing='ij',
+    )
+
+    cost = np.zeros_like(F)
+
+    p = tqdm(total=np.prod(N.shape))
+
+    for i in range(N.shape[0]):
+        for j in range(N.shape[1]):
+            cost[i,j] = get_param_cost(N[i,j], F[i,j])
+            p.update(1)
+
+    p.close()
+
+    return N, F, cost
+
+
+if __name__=='__main__':
+    #test_scheduling_set()
+
+    n_steps = np.array(list(range(1,10)) + [20,30,40,50,100])
+    f_steps = np.linspace(0.1,1,num=20)
+
+    N, F, cost = grid_search_cost_function(n_steps, f_steps)
+    fig, ax = plt.subplots()
+
+    c = ax.pcolormesh(N, F, cost)
+    ax.set_title('Cost function')
+    fig.colorbar(c, ax=ax)
+
+    plt.show()
