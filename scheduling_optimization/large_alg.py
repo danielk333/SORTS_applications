a91123d086f79b59b0be810bf83114e88eb757f9
danielk <daniel.kastinen@irf.se>
2021-06-09 10:05:52 +0200

    added more mock code

--- scheduling_optimization/large_alg.py
+++ scheduling_optimization/large_alg.py
@@ -1,29 +1,72 @@
 
+def distribute_points(points, Sigma_orbs, params, **conditions):
+    '''EXAMPLE ????
+    '''
+    
+    points_avalible = points.sum() - conditions['max_points']
+
+    order = conditions['priorities'].argsort()
+
+    points_cache = conditions.get('points_cache', 100)
+
+    #we need to remove points
+    if points_avalible < 0:
+        order = order[::-1]
+
+        for i in order:
+            Sigma = Sigma_orbs[i]:
+            if object_lost(Sigma):
+                if points_avalible < points_cache
+                    points_avalible += points[i]
+                    points[i] = 0
+
+        order = order[::-1]
+
+    kept_objs = 0
+
+    for i in order:
+        Sigma = Sigma_orbs[i]:
+        if object_lost(Sigma):
+            if points_avalible > 0:
+                points_avalible -= 1
+                points[i] += 1
+        else:
+            kept_objs += 1
+
+    done = kept_objs > params['x']*len(Sigma_orbs)
+
+    return points, done
+
 def set_schedule(scheduler, points):
+    cnt = 0
     for i, obj in enumerate(scheduler.pop):
-    passes = scheduler.get_passes(obj)
-    for ps in passes:
-        scheduler.update_tracking(obj, ps, tracklet_points=points[i])
+        passes = scheduler.get_passes(obj)
+        scheduler.update_tracking(obj, passes, tracklet_points=points[cnt])
+
 
 
 def generate_schedule(scheduler, params, conditions):
 
-    points = np.empty((len(scheduler.pop),), dtype=int)
+    points = np.empty((len(scheduler.trackers),), dtype=int)
     points[:] = 2
 
     set_schedule(scheduler, points)
     Sigma_orbs = scheduler.get_od_errors()
 
     done = False
+    iters = 0
     while not done:
         points, done = distribute_points(points, Sigma_orbs, params, **conditions)
         set_schedule(scheduler, points)
         Sigma_orbs = scheduler.get_od_errors()
+        iters += 1
+        if iters > 10:
+            break
 
     return Sigma_orbs, points
 
 
-def cost_function(Sigma_orbs, priorities):
+def cost_function(Sigma_orbs, points, priorities):
     c = 0
     tracked = 0
     for Sigma, prio in zip(Sigma_orbs, priorities):
@@ -42,9 +85,12 @@ def find_schedule(scheduler, priorities):
         Sigma_orbs, points = generate_schedule(
             scheduler, 
             params = {'x': x},
-            conditions = {'max_points': 93747},
+            conditions = {
+                'max_points': 93747,
+                'priorities': priorities,
+            },
         )
-        return cost_function(Sigma_orbs, priorities)
+        return cost_function(Sigma_orbs, points, priorities)
 
     x = fminsearch(func, x=0.1)
 
