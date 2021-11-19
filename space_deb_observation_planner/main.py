1ffdb653156dd544bcb983fdaa70a9984d7ab05c
danielk <daniel.kastinen@irf.se>
2021-11-19 18:23:26 +0100

    getting to data products for planner, now much validate and determine good outputs

--- space_deb_observation_planner/main.py
+++ space_deb_observation_planner/main.py
@@ -11,6 +11,10 @@ import argparse
 import sys
 import subprocess
 import shutil
+import pickle
+import multiprocessing as mp
+from tqdm import tqdm
+import time
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -25,7 +29,8 @@ import ndm_plotting
 
 HERE = pathlib.Path(__file__).parent.resolve()
 CACHE = HERE / '.cache'
-MODEL_DIR = CACHE  / 'nasa-breakup-model'
+MODEL_DIR = CACHE / 'nasa-breakup-model'
+
 
 def check_setup():
     bin_file = CACHE / 'nasa-breakup-model' / 'bin' / 'breakup'
@@ -45,6 +50,83 @@ def setup():
     
     subprocess.check_call(['make'], cwd=str(MODEL_DIR / 'src'))
 
+
+def process_orbit(
+            state,
+            parameters,
+            epoch,
+            t,
+            propagator,
+            propagator_options,
+            scheduler,
+            obs_epoch,
+            num,
+        ):
+    space_orbit = sorts.SpaceObject(
+        propagator,
+        propagator_options = propagator_options,
+        x = state[0],
+        y = state[1],
+        z = state[2],
+        vx = state[3],
+        vy = state[4],
+        vz = state[5],
+        epoch = epoch,
+        parameters = parameters,
+    )
+    space_orbit.in_frame = 'TEME'
+    space_orbit.out_frame = 'ITRS'
+    space_orbit.orbit.type = 'mean'
+    space_orbit.orbit.degrees = True
+
+    odatas = []
+    for anom in tqdm(np.linspace(0, 360.0, num=num)):
+        space_orbit.orbit.anom = anom
+
+        states_orb = space_orbit.get_state(t)
+        interpolator = sorts.interpolation.Legendre8(states_orb, t)
+        scheduler.set_tracker(t, states_orb)
+        odatas += [scheduler.observe_passes(
+            scheduler.passes, 
+            space_object = space_orbit, 
+            epoch = obs_epoch, 
+            interpolator = interpolator,
+            snr_limit = False,
+        )]
+    return odatas
+
+
+def process_fragment(
+            fragment,
+            fragmentation_epoch,
+            t,
+            propagator,
+            propagator_options,
+            scheduler,
+            obs_epoch,
+        ):
+    space_fragment = sorts.SpaceObject(
+        propagator,
+        propagator_options = propagator_options,
+        state = fragment[4:10],
+        epoch = fragmentation_epoch,
+        parameters = dict(
+            m = fragment[11],
+            d = fragment[12],
+        ),
+    )
+
+    states_fragments = space_fragment.get_state(t)
+    interpolator = sorts.interpolation.Legendre8(states_fragments, t)
+    scheduler.set_tracker(t, states_fragments)
+    fdata = scheduler.observe_passes(
+        scheduler.passes, 
+        space_object = space_fragment, 
+        epoch = obs_epoch, 
+        interpolator = interpolator,
+        snr_limit = False,
+    )
+    return fdata, states_fragments
 
 
 def run_nasa_breakup(nbm_param_file, nbm_param_type, space_object, fragmentation_time):
@@ -73,7 +155,13 @@ def run_nasa_breakup(nbm_param_file, nbm_param_type, space_object, fragmentation
     )
     kepler_teme = orbit_teme.kepler.flatten()
 
-    sched_header = ['ID_NORAD', 'J_DAY', 'YYYY', 'MM', 'DD', 'NAME', 'TYPE', 'SMA[m]', 'ECC', 'INC[deg]', 'RAAN[deg]', 'A_PER[deg]', 'MA[deg]', 'MASS[kg]', 'S']
+    sched_header = [
+        'ID_NORAD', 'J_DAY', 'YYYY', 
+        'MM', 'DD', 'NAME', 
+        'TYPE', 'SMA[m]', 'ECC', 
+        'INC[deg]', 'RAAN[deg]', 'A_PER[deg]', 
+        'MA[deg]', 'MASS[kg]', 'S',
+    ]
     sched_data = [
         '00000001', 
         f'{event_time.jd:.6f}', 
@@ -118,11 +206,13 @@ def run_nasa_breakup(nbm_param_file, nbm_param_type, space_object, fragmentation
 
     return cloud_data, header
 
+
 class TrackingScheduler(
-        sorts.scheduler.StaticList, 
-        sorts.scheduler.ObservedParameters,
-    ):
-    def __init__(self, radar, t, states, dwell=0.1, profiler=None, logger=None, **kwargs):
+            sorts.scheduler.StaticList, 
+            sorts.scheduler.ObservedParameters,
+        ):
+
+    def __init__(self, radar, dwell=0.1, profiler=None, logger=None, **kwargs):
         self.dwell = dwell
         super().__init__(
             radar=radar, 
@@ -131,8 +221,6 @@ class TrackingScheduler(
             profiler=profiler,
             **kwargs
         )
-        self.set_tracker(t, states)
-
 
     def set_tracker(self, t, states):
         self.passes = self.radar.find_passes(t, states)
@@ -162,9 +250,7 @@ class TrackingScheduler(
                 self.controllers.append(tracker)
 
 
-
-
-if __name__=='__main__':
+if __name__ == '__main__':
 
     if not check_setup():
         setup()
@@ -186,6 +272,8 @@ if __name__=='__main__':
     config = configparser.ConfigParser(interpolation=None)
     config.read([args.config])
 
+    cores = config.getint('general', 'cores', fallback=0)
+
     line1 = config.get('orbit', 'line 1', fallback=None)
     line2 = config.get('orbit', 'line 2', fallback=None)
 
@@ -194,7 +282,7 @@ if __name__=='__main__':
             (
                 line1,
                 line2,
-             ),
+            ),
         ]
         print('Using TLEs')
     else:
@@ -217,7 +305,7 @@ if __name__=='__main__':
         A = config.getfloat('orbit', 'a', fallback=None),
         m = config.getfloat('orbit', 'm', fallback=None),
     )
-    parameters = {key:val for key,val in parameters.items() if val is not None}
+    parameters = {key: val for key, val in parameters.items() if val is not None}
 
     if args.propagator == 'SGP4':
         if 'fragmentation' not in args.target and 'orbit' not in args.target:
@@ -277,7 +365,7 @@ if __name__=='__main__':
             propagator_options = propagator_options,
         )
         if sgp4_propagation:
-            pop.propagator_options['settings']['out_frame']='ITRS'
+            pop.propagator_options['settings']['out_frame'] = 'ITRS'
         space_object = pop.get_object(0)
         space_object.parameters.update(parameters)
 
@@ -321,6 +409,9 @@ if __name__=='__main__':
             fragmentation_time,
         )
 
+        fdt = TimeDelta(fragmentation_time*3600.0, format='sec')
+        fragmentation_epoch = space_object.epoch + fdt
+
         nbm_folder = output / 'nbm_results'
         nbm_folder.mkdir(parents=True, exist_ok=True)
         nbm_results = (MODEL_DIR / 'bin' / 'outputs').glob('*.txt')
@@ -349,21 +440,23 @@ if __name__=='__main__':
     logger = sorts.profiling.get_logger()
 
     t = np.arange(t_start, t_end, t_step)
+    print(f'T=[{t[0]} s -> @ {len(t)} x {t_step} s steps -> {t[-1]} s]')
     states = space_object.get_state(t)
 
     scheduler = TrackingScheduler(
         radar = radar, 
-        t = t,
-        states = states, 
         profiler = profiler, 
         logger = logger,
     )
+    scheduler.set_tracker(t, states)
+    interpolator = sorts.interpolation.Legendre8(states, t)
 
     data = scheduler.observe_passes(
         scheduler.passes, 
-        space_object=space_object, 
-        epoch=obs_epoch, 
-        snr_limit=False,
+        space_object = space_object, 
+        epoch = obs_epoch, 
+        interpolator = interpolator,
+        snr_limit = False,
     )
 
     out_data_txt = ''
@@ -373,11 +466,223 @@ if __name__=='__main__':
             for pi, ps in enumerate(scheduler.passes[txi][rxi]):
                 ps_start = obs_epoch + TimeDelta(ps.start(), format='sec')
                 ps_end = obs_epoch + TimeDelta(ps.end(), format='sec')
-                out_data_txt += f'TX={txi} | RX={rxi} | Pass {pi} | Start: {ps_start.iso} -> End: {ps_end.iso}\n'
+                out_data_txt += f'TX={txi} | RX={rxi} | Pass {pi} |'
+                out_data_txt += f'Start: {ps_start.iso} -> End: {ps_end.iso}\n'
 
-    #plot results
-    fig1 = plt.figure(figsize=(15,15))
-    fig2 = plt.figure(figsize=(15,15))
+    if 'orbit' in args.target:
+        __out_frame = space_object.out_frame
+        space_object.out_frame = 'TEME'
+        orbit_state = space_object.get_state([0.0])
+        space_object.out_frame = __out_frame
+
+        odatas = process_orbit(
+            orbit_state,
+            space_object.parameters,
+            space_object.epoch,
+            t,
+            propagator,
+            propagator_options,
+            scheduler,
+            obs_epoch,
+            config.getint('orbit', 'samples', fallback=300),
+        )
+        with open(output / 'orbit_data.pickle', 'wb') as fh:
+            pickle.dump(odatas, fh)
+
+        FIX THIS
+        sn = []
+        t = []
+        for oid in range(len(orbit_data)):
+            for ind, d in enumerate(orbit_data[oid][0][0]):
+                snm = np.argmax(d['snr'])
+                sn.append(d['snr'][snm])
+                t.append(d['t'][snm])
+        t = np.array(t)
+        sn = np.array(t)
+
+        t_sort = np.argsort(t)
+        t = t[t_sort]
+        sn = sn[t_sort]
+
+        fig, ax = plt.subplots()
+        ax.plot(t/3600.0, 10*np.log10(sn), '.b')
+        ax.set_ylabel('Orbit SNR [dB]')
+        ax.set_xlabel('Time past epoch [h]')
+
+
+
+    if 'fragmentation' in args.target:
+        fragment_scheduler = TrackingScheduler(
+            radar = radar, 
+            profiler = profiler, 
+            logger = logger,
+        )
+
+        propagator_options['settings']['in_frame'] = 'TEME'
+        fragment_data = []
+        fragment_states = {}
+
+        pbar = tqdm(total=len(cloud_data), desc='Processing fragments')
+
+        if cores > 0:
+            pool = mp.Pool(cores)
+            reses = []
+
+            for fid, fragment in enumerate(cloud_data):
+                reses.append(pool.apply_async(
+                    process_fragment, 
+                    args=(
+                        fragment,
+                        fragmentation_epoch,
+                        t,
+                        propagator,
+                        propagator_options,
+                        fragment_scheduler,
+                        obs_epoch,
+                    ), 
+                ))
+            pool_status = np.full((len(cloud_data), ), False)
+            while not np.all(pool_status):
+                for fid, res in enumerate(reses):
+                    if pool_status[fid]:
+                        continue
+                    time.sleep(0.01)
+                    _ready = res.ready()
+                    if not pool_status[fid] and _ready:
+                        pool_status[fid] = _ready
+                        pbar.update()
+
+            for fid, res in enumerate(reses):
+                fdata, states_fragments = res.get()
+                fragment_data.append(fdata)
+                fragment_states[f'{fid}'] = states_fragments
+
+            pool.close()
+            pool.join()
+
+        else:
+            for fid, fragment in enumerate(cloud_data):
+                pbar.update()
+                fdata, states_fragments = process_fragment(
+                    fragment,
+                    fragmentation_epoch,
+                    t,
+                    propagator,
+                    propagator_options,
+                    fragment_scheduler,
+                    obs_epoch,
+                )
+
+                fragment_data.append(fdata)
+                fragment_states[f'{fid}'] = states_fragments
+            
+        pbar.close()
+        with open(output / 'fragment_data.pickle', 'wb') as fh:
+            pickle.dump(fragment_data, fh)
+        np.savez(output / 'fragment_states.npz', **fragment_states)
+
+        fragment_pass_data = []
+        for txi in range(len(radar.tx)):
+            fragment_pass_data.append([])
+            for rxi in range(len(radar.rx)):
+                fragment_pass_data[txi].append(dict(
+                    peak_snr = [],
+                    peak_time = [],
+                ))
+                for fid, fdata in enumerate(fragment_data):
+                    for fps in fdata[txi][rxi]:
+                        max_sn_ind = np.argmax(fps['snr'])
+                        fragment_pass_data[txi][rxi]['peak_snr'].append(
+                            fps['snr'][max_sn_ind]
+                        )
+                        fragment_pass_data[txi][rxi]['peak_time'].append(
+                            fps['t'][max_sn_ind]
+                        )
+
+        with open(output / 'fragment_pass_data.pickle', 'wb') as fh:
+            pickle.dump(fragment_pass_data, fh)
+
+        # plot results
+        fig_frag = plt.figure(figsize=(15, 15))
+        ax = fig_frag.add_subplot(111, projection='3d')
+        sorts.plotting.grid_earth(ax)
+        for fid, states_fragments in fragment_states.items():
+            ax.plot(states_fragments[0, :], states_fragments[1, :], states_fragments[2, :], 'b', alpha=0.2)
+            ax.plot(states_fragments[0, -1], states_fragments[1, -1], states_fragments[2, -1], '.r', alpha=1)
+        sorts.plotting.set_axes_equal(ax)
+
+        fig_frag.savefig(output / 'fragment_orbits.png')
+        plt.close(fig_frag)
+
+        if config.getboolean('fragmentation', 'animate', fallback=False):
+            output_anim = output / 'animation'
+            output_anim.mkdir(exist_ok=True)
+
+            fig_frag = plt.figure(figsize=(15, 15))
+            ax = fig_frag.add_subplot(111, projection='3d')
+            
+            max_offset = 10
+            step_size = 100
+            for tid in tqdm(range(0, len(t), step_size)):
+                if tid < max_offset:
+                    offset = tid
+                else:
+                    offset = offset
+                ax.clear()
+                sorts.plotting.grid_earth(ax)
+                for fid, states_fragments in fragment_states.items():
+                    ax.plot(
+                        states_fragments[0, (tid - offset):tid], 
+                        states_fragments[1, (tid - offset):tid], 
+                        states_fragments[2, (tid - offset):tid], 
+                        'b', alpha=0.2,
+                    )
+                    ax.plot(
+                        states_fragments[0, tid], 
+                        states_fragments[1, tid], 
+                        states_fragments[2, tid], 
+                        '.r', alpha=1,
+                    )
+                if tid == 0:
+                    sorts.plotting.set_axes_equal(ax)
+                fig_frag.savefig(output_anim / f'frag_anim{tid}.png')
+            
+            plt.close(fig_frag)
+
+            try:
+                conv_path = pathlib.Path(".") / "animation" / "*.png"
+                subprocess.check_call(
+                    f'convert -delay 20 -loop 0 {str(conv_path)} animation.gif', 
+                    cwd=str(output),
+                )
+            except subprocess.CalledProcessError as e:
+                print(e)
+                print('Could not create gif from animation frames... probably ImageMagick is missing')
+
+        FIX THIS
+        sn = np.array(data[0][0]['peak_snr'])
+        t = np.array(data[0][0]['peak_time'])
+
+        fig, axes = plt.subplots(1,3, figsize=(12,6))
+        ax = axes[0]
+        ax.hist(10*np.log10(sn[sn > 1]))
+        ax.set_xlabel('Max SNR [dB]')
+        ax.set_ylabel('Passes')
+
+        ax = axes[1]
+        ax.hist(t[sn > 1]/3600.0)
+        ax.set_xlabel('Time past epoch [h]')
+        ax.set_ylabel('Passes')
+
+        ax = axes[2]
+        ax.plot(t[sn > 1]/3600.0, 10*np.log10(sn[sn > 1]), '.b')
+        ax.set_xlabel('Time past epoch [h]')
+        ax.set_ylabel('Max SNR [dB]')
+
+
+    # plot results
+    fig1 = plt.figure(figsize=(15, 15))
+    fig2 = plt.figure(figsize=(15, 15))
     axes = []
     r_axes = []
     sn_axes = []
@@ -398,10 +703,9 @@ if __name__=='__main__':
         ax.set_xlabel('k_x [East]')
         ax.set_ylabel('k_y [North]')
         ax.set_title(f'Receiver station {rxi}')
-        ax.set_xlim([-1,1])
-        ax.set_ylim([-1,1])
+        ax.set_xlim([-1, 1])
+        ax.set_ylim([-1, 1])
         
-
     for rxi, ax in enumerate(sn_axes):
         ax.set_xlabel('Time during pass [d]')
         ax.set_ylabel('SNR [dB/h]')
@@ -413,16 +717,16 @@ if __name__=='__main__':
         ax.set_ylabel('Range [km]')
         ax.set_title(f'Receiver station {rxi}')
 
-    #plot results
-    fig3 = plt.figure(figsize=(15,15))
+    # plot results
+    fig3 = plt.figure(figsize=(15, 15))
     ax = fig3.add_subplot(111, projection='3d')
-    ax.plot(states[0,:]*1e-3, states[1,:]*1e-3, states[2,:]*1e-3, 'b')
+    ax.plot(states[0, :]*1e-3, states[1, :]*1e-3, states[2, :]*1e-3, 'b')
     for ctrl in scheduler.controllers:
         for ti in range(len(ctrl.t)):
             ax.plot(
-                [radar.tx[0].ecef[0]*1e-3, ctrl.ecefs[0,ti]*1e-3], 
-                [radar.tx[0].ecef[1]*1e-3, ctrl.ecefs[1,ti]*1e-3], 
-                [radar.tx[0].ecef[2]*1e-3, ctrl.ecefs[2,ti]*1e-3], 
+                [radar.tx[0].ecef[0]*1e-3, ctrl.ecefs[0, ti]*1e-3], 
+                [radar.tx[0].ecef[1]*1e-3, ctrl.ecefs[1, ti]*1e-3], 
+                [radar.tx[0].ecef[2]*1e-3, ctrl.ecefs[2, ti]*1e-3], 
                 'g',
             )
 
@@ -432,9 +736,15 @@ if __name__=='__main__':
     fig1.savefig(output / 'kvec_snr.png')
     fig2.savefig(output / 'range.png')
     fig3.savefig(output / '3d_obs.png')
+    plt.close(fig1)
+    plt.close(fig2)
+    plt.close(fig3)
 
     with open(output / 'cmd.txt', 'w') as fh:
         fh.write(" ".join(sys.argv))
 
     with open(output / 'data.txt', 'w') as fh:
         fh.write(out_data_txt)
+
+    with open(output / 'passes.pickle', 'wb') as fh:
+        pickle.dump(scheduler.passes, fh)
